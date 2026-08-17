66d1d27610b5d63486d8608d7c254755645f173d
saifulasamad80 <saifulasamad80@gmail.com>
2026-08-17 21:45:31 +0700

    fix: flush service worker cache and enforce direct network fetch

--- public/sw.js
+++ public/sw.js
@@ -1,13 +1,6 @@
-self.addEventListener('install', (e) => {
-  self.skipWaiting();
-});
-
+self.addEventListener('install', () => self.skipWaiting());
 self.addEventListener('activate', (e) => {
-  e.waitUntil(clients.claim());
-});
-
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
+  e.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => caches.delete(k))))
   );
 });
